7c64643766b13cf0183f9ea7e7a70f1f5f0ddb24
Jacob Gotcher <jacob.gotcher@gmail.com>
2019-02-18 16:20:10 -0600

    Add files via upload

---
+++ ThreeSolidPrinciples/src/manager.java
@@ -0,0 +1,24 @@
+package threesolid;
+
+import javax.net.ssl.ManagerFactoryParameters;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.;
+
+
+class ProjectManager implements schedulework extends Manager {
+
+    schedulework = schedulework();{
+        //...more
+    }
+
+}
+
+class ProductManager implements defineproduct extends Manager {
+
+    defineproduct = defineproduct();{
+        //...more
+    }
+
+}
+
