a4b94975e445eb7845103b5290a848275b5f9272
pmc44 <46945240+pmc44@users.noreply.github.com>
2019-02-18 23:14:07 -0600

    Recommit (Assignment 5: 8a and 8b)

--- ThreeSolidPrinciples/src/ISick.java
+++ ThreeSolidPrinciples/src/ISick.java
@@ -1,4 +1,8 @@
+package threesolid;
 
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+import java.io.*;
 /**
  * ISick interface
  *
