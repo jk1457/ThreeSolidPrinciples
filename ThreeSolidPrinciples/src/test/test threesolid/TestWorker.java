42d6d00cdfe6d79fe4b2b82e45afc5995ee0b253
Cavcode <cameronav23@gmail.com>
2019-04-25 18:02:56 -0500

    Bunch of changes

--- ThreeSolidPrinciples/src/test/test threesolid/TestWorker.java
+++ ThreeSolidPrinciples/src/test/test threesolid/TestWorker.java
@@ -8,11 +8,18 @@ import org.junit.jupiter.api.Test;
 public class TestWorker {
 
 	private Worker w = new Worker();
-	
+
 	@Test
 	@DisplayName("Work Method Test")
 	public void testWorkMethod()
 	{
 		assertEquals(w.work(), "I'm working already!","Mismatch between test text and method text");
+	}
+
+	@Test
+	@DisplayName("Work Method Test 2")
+	public void testWorkMethod()
+	{
+		assertEquals(w.work(), "I'm working already!","I don't wanna work!");
 	}
 }
